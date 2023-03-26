2a316c915cd3ba39e06d9169a1e37159df44c485
Saad5400 <sdbtwa@gmail.com>
2023-03-26 23:06:31 +0300

    Add project files.

---
+++ StudentsClubsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentsClubsWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
