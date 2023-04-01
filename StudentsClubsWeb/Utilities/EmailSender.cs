18176dd4cdc2751f9b821ae4e13163e2b7caa81b
Saad5400 <sdbtwa@gmail.com>
2023-04-01 08:45:57 +0300

    added email sender

--- StudentsClubsWeb/Utilities/EmailSender.cs
+++ StudentsClubsWeb/Utilities/EmailSender.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 
 namespace StudentsClubsWeb.Utilities
 {
@@ -6,6 +8,23 @@ namespace StudentsClubsWeb.Utilities
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("saaddev5400@gmail.com"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = htmlMessage
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                client.Authenticate("saaddev5400@gmail.com", "saadbatwa");
+                client.Send(emailToSend);
+                client.Disconnect(true);
+            }
+
             return Task.CompletedTask;
         }
     }
